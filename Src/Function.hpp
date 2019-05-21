e53f6cbcac3e8e399312e355ebc93a507899620e
pamplemousseMR <romain.manciaux@gmail.com>
2019-05-21 21:03:02 +0200

    feat(Function): add main function

--- Src/Function.hpp
+++ Src/Function.hpp
@@ -6,6 +6,8 @@
 
 #include <string>
 
+#include "Operator.hpp"
+
 /* ---------------------------------------------------------------------------------
 										Declaration
 --------------------------------------------------------------------------------- */
@@ -15,6 +17,8 @@ class Function
 
 public:
 
+	Function(const Operator< float >&);
+
 	Function(const Function&) = default;
 
 	Function& operator=(const Function&) = delete;
@@ -23,13 +27,29 @@ public:
 
 	Function& operator=(Function&&) = default;
 	
+	float compute() const;
+
 	friend std::ostream& operator <<(std::ostream& _o, const Function& _t)
 	{
 		return _o << "TODO";
 	}
+
+private:
+
+	Operator< float > m_operator;	
 
 };
 
 /* ---------------------------------------------------------------------------------
 										Definition
 --------------------------------------------------------------------------------- */
+
+Function::Function(const Operator< float >& _operator) :
+	m_operator(_operator)
+{
+}
+
+float Function::compute() const
+{
+	return m_operator.compute();
+}
